240a34a6a3789bbda3f705ce68d89038b28a4b0d
Thiago <thiago_ssfr@hotmail.com>
2021-07-27 17:50:53 -0300

    Exercicio Triangulo

---
+++ Fundamentos_Web/introducao-javascript/4.1/maiorde2numeros.js
@@ -0,0 +1,13 @@
+let num1 = 10;
+let num2 = 2;
+
+if(num1 > num2){
+    console.log(num1 +" é maior que "+num2);
+}else if(num1 < num2){
+    console.log(num2 +" é maior que "+num1);
+}else if(num1 === num2){
+    console.log("Os dois Números são iguais");
+}else{
+    console.log("Valor incorreto");
+}
+
