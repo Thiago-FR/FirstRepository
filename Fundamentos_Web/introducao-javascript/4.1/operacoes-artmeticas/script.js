25c259633cb27035b89c5b43456f6598ecdfb1aa
Thiago <thiago_ssfr@hotmail.com>
2021-07-27 12:03:17 -0300

    operacoes aritmetica

---
+++ Fundamentos_Web/introducao-javascript/4.1/operacoes-artmeticas/script.js
@@ -0,0 +1,20 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+let base = 5;
+
+let altura = 8;
+
+area = base * altura;
+
+let perimetro = (5*2) + (8*2);
+
+
+console.log("Area = "+area);
+console.log("Perimetro = "+perimetro);
+
