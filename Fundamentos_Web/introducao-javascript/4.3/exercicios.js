a9a7137b580adbb5e1d1761a4cf36ed1565b2e6c
Thiago <thiago_ssfr@hotmail.com>
2021-07-29 11:38:34 -0300

    Exercicios 4.3/1

---
+++ Fundamentos_Web/introducao-javascript/4.3/exercicios.js
@@ -0,0 +1,18 @@
+//Exercicio 1
+console.log("------Exercicios 1-------");
+let n = 5;
+let cont = "";
+
+ for(let i = 0; i < n; i += 1){
+   cont = "";
+   for(let j = 0; j < n; j += 1){
+      cont += "*";
+  }
+  console.log(cont);
+  
+}
+
+
+
+
+
