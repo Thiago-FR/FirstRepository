da333c20a8a731451f6e319be2737a55eff9add6
Thiago <thiago_ssfr@hotmail.com>
2021-07-29 15:28:06 -0300

    Exercicios Bonus 4.3

--- Fundamentos_Web/introducao-javascript/4.3/exercicios.js
+++ Fundamentos_Web/introducao-javascript/4.3/exercicios.js
@@ -40,5 +40,74 @@ for(let i = 0; i < n3; i += 1){
  console.log(cont3);  
 }
 
+//Exercicio 4
+console.log("------Exercicios 4-------");
+let n4 = 5;
 
+let medio = (n4 + 1) / 2;
+let esq = medio;
+let dir = medio;
+let cont4 = "";
+
+for(let i = 0; i <= medio; i+=1){
+  cont4 = "";
+  for(let j = 0; j <= n4; j +=1){
+    if(j > esq && j < dir){
+      cont4 += "*"
+    }else{
+      cont4 += " ";
+    }
+  }
+  console.log(cont4);  
+  dir += 1;
+  esq -= 1;
+}
+
+
+//Exercicio 5
+console.log("------Exercicios 5 Bonus-------");
+
+let n5 = 5;
+
+let medio5 = (n4 + 1) / 2;
+let esq5 = medio5;
+let dir5 = medio5;
+let cont5 = "";
+
+for(let i = 1; i <= medio5; i+=1){
+  cont5 = "";
+  for(let j = 1; j <= n5; j +=1){
+    if(j == esq5 || j == dir5 || i == medio5){
+      cont5 += "*"
+    }else{
+      cont5 += " ";
+    }
+  }
+  console.log(cont5);  
+  dir5 += 1;
+  esq5 -= 1;
+}
+
+//Exercicio 6
+console.log("------Exercicios 6 Bonus-------");
+let n6 = 391;
+let cont6 = 0;
+let div6 = n6;
+let result6 = 0;
+
+for(let i = 0; i <= n6; i +=1){
+    result6 = n6%div6;
+    if(result6 == 0){
+      cont6 +=1;
+    }
+    div6 -= 1;
+}
+
+if(cont6 === 2 ){
+  console.log(cont6);
+  console.log("É um número primo");
+}else{
+  console.log(cont6);
+  console.log("Não é um número primo");
+}
 
