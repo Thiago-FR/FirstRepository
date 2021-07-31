e12fe0aeb490f3c5717411ec3617662e2cc7fc02
Thiago <thiago_ssfr@hotmail.com>
2021-07-31 15:53:28 -0300

    Exercicio 3 Bonus

--- Fundamentos_Web/introducao-javascript/4.4/bonus.js
+++ Fundamentos_Web/introducao-javascript/4.4/bonus.js
@@ -81,3 +81,51 @@ function arrayOfNumbers(vector){
 console.log();
 
 console.log("---------Exercício 3 Bonus----------");
+
+
+
+const basket = [
+  'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+  'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+  'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+  'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+  'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+  'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+  'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+  'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+  'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+  'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+  'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+  'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+  'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+  'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+  'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+  'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+  'Banana', 'Pera', 'Abacate', 'Uva',
+];
+
+let cesta = {
+  
+}
+
+verificaCesta(basket,cesta);
+
+function verificaCesta(basket,cesta){
+  for(let index in basket){
+    let temNaCesta = false;
+    for(let j in cesta){    
+      if(basket[index] == j){
+        cesta[j] += 1;
+        temNaCesta = true;
+        break;
+      }    
+    }
+    if(temNaCesta == false){
+      cesta[basket[index]] = 1;
+    }
+  }
+  console.log(cesta);
+}
+
+
+
