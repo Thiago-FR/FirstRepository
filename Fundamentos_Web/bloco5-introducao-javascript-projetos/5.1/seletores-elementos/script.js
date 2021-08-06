53063be84721a13a9bec5655a850ab11ce9226b8
Thiago <thiago_ssfr@hotmail.com>
2021-08-06 16:10:49 -0300

    Substuicao por querySelector

--- Fundamentos_Web/bloco5-introducao-javascript-projetos/5.1/seletores-elementos/script.js
+++ Fundamentos_Web/bloco5-introducao-javascript-projetos/5.1/seletores-elementos/script.js
@@ -1,4 +1,4 @@
-const paragraph = document.getElementById("paragraph");
+/*const paragraph = document.getElementById("paragraph");
       paragraph.style.color = "red";
 
 console.log(document.getElementById("page-title").innerText);
@@ -18,5 +18,28 @@ let clas = document.getElementsByClassName("equals");
     }
   
 let subTitle = document.getElementsByTagName("h2");
-    subTitle[0].style.color = "red";
+    subTitle[0].style.color = "red";*/
+
+    const paragraph = document.querySelector("#paragraph");
+      paragraph.style.color = "red";
+
+console.log(document.querySelector("#page-title").innerText);
+
+let segundoParagrafo = document.querySelector("#second-paragraph");
+    segundoParagrafo.style.color = "red";
+    segundoParagrafo.style.backgroundColor = "blue";
+    segundoParagrafo.style.fontSize = "30px";
+    segundoParagrafo.style.textAlign = "center"
+
+let clas = document.querySelectorAll(".equals");
+    clas[0].style.color = "blue";
+    clas[1].style.color = "green";
+
+    for(let i = 0; i < clas.length; i += 1){
+      clas[i].innerText = "Mudei de nome";
+    }
+  
+let subTitle = document.querySelector("h2");
+    subTitle.style.color = "red";
+
   
