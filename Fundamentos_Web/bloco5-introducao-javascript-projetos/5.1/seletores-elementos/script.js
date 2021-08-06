c9734574bee51dec5c991132ec33cdd166180147
Thiago <thiago_ssfr@hotmail.com>
2021-08-06 15:58:56 -0300

    Para fixar seletores de elementos

---
+++ Fundamentos_Web/bloco5-introducao-javascript-projetos/5.1/seletores-elementos/script.js
@@ -0,0 +1,22 @@
+const paragraph = document.getElementById("paragraph");
+      paragraph.style.color = "red";
+
+console.log(document.getElementById("page-title").innerText);
+
+let segundoParagrafo = document.getElementById("second-paragraph");
+    segundoParagrafo.style.color = "red";
+    segundoParagrafo.style.backgroundColor = "blue";
+    segundoParagrafo.style.fontSize = "30px";
+    segundoParagrafo.style.textAlign = "center"
+
+let clas = document.getElementsByClassName("equals");
+    clas[0].style.color = "blue";
+    clas[1].style.color = "green";
+
+    for(let i = 0; i < clas.length; i += 1){
+      clas[i].innerText = "Mudei de nome";
+    }
+  
+let subTitle = document.getElementsByTagName("h2");
+    subTitle[0].style.color = "red";
+  
