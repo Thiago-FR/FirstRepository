b5a87ce7eef0cea6fa913bd1da7346cfabd78899
Thiago <thiago_ssfr@hotmail.com>
2021-08-10 20:59:56 -0300

    Execicio 2

--- Fundamentos_Web/bloco5-introducao-javascript-projetos/5.3/script.js
+++ Fundamentos_Web/bloco5-introducao-javascript-projetos/5.3/script.js
@@ -12,27 +12,41 @@ function createDaysOfTheWeek() {
 };
 
 createDaysOfTheWeek();
-
+createDays();
+btnFeriado("Feriados");
 // Escreva seu código abaixo.
 
 //#1
-const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-const weekDays = document.querySelector('#days');
-for(let index = 0; index < dezDaysList.length; index += 1){
-  let days = dezDaysList[index];  
-  let daysListItem = document.createElement('li');  
-  daysListItem.innerHTML = days;  
-  if(index === 0 || index === 1){
-    weekDays.appendChild(daysListItem); 
-  }else if(days === 24 || days === 25 || days === 31){
-    daysListItem.className = 'day holiday'; 
-    weekDays.appendChild(daysListItem);   
-  }else if(days === 4 || days === 11 || days === 18 || days === 25){
-    daysListItem.className = 'day friday';
-    weekDays.appendChild(daysListItem); 
-  }else{
-    weekDays.appendChild(daysListItem);
-  }
-  
-   
+function createDays(){
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const weekDays = document.querySelector('#days');
+  for(let index = 0; index < dezDaysList.length; index += 1){
+    let days = dezDaysList[index];  
+    let daysListItem = document.createElement('li');  
+    daysListItem.innerHTML = days;  
+    if(index === 0 || index === 1){
+      weekDays.appendChild(daysListItem); 
+    }else if(days === 24 || days === 25 || days === 31){
+      daysListItem.className = 'day holiday'; 
+      weekDays.appendChild(daysListItem);   
+    }else if(days === 4 || days === 11 || days === 18 || days === 25){
+      daysListItem.className = 'day friday';
+      weekDays.appendChild(daysListItem); 
+    }else{
+      weekDays.appendChild(daysListItem);
+    }   
+}
+}
+
+//#2
+
+
+let feriado = 'Feriados';
+
+function btnFeriado(string){
+  let btnContainer = document.querySelector('.buttons-container');
+  let btnHoliday = document.createElement('button');
+  btnHoliday.id = 'btn-holiday';
+  btnHoliday.innerHTML = string;
+  btnContainer.appendChild(btnHoliday);
 }
