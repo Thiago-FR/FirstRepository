0f0531254f9ecd1894a8bd190a3ad3607ff1ea67
Thiago <thiago_ssfr@hotmail.com>
2021-08-15 21:55:06 -0300

    Requisito 1 e 2 Exercício

---
+++ Fundamentos_Web/bloco6-html-css-forms/6.1/script.js
@@ -0,0 +1,37 @@
+const estados = [
+  'Acre',
+  'Alagoas',
+  'Amapá',
+  'Amazonas',
+  'Bahia',
+  'Ceará',
+  'Distrito Federal',
+  'Espírito Santo',
+  'Goiás',
+  'Maranhão',
+  'Mato Grosso',
+  'Mato Grosso do Sul',
+  'Minas Gerais',
+  'Pará',
+  'Paraíba',
+  'Paraná',
+  'Pernambuco',
+  'Piauí',
+  'Rio de Janeiro',
+  'Rio Grande do Norte',
+  'Rio Grande do Sul',
+  'Rondônia',
+  'Roraima',
+  'Santa Catarina',
+  'São Paulo',
+  'Sergipe',
+  'Tocantins'
+]
+
+const estadosSelect = document.getElementById('estado-select');
+
+for(let index = 0; index < estados.length; index += 1){
+  const estadoBr = document.createElement('option');
+  estadoBr.innerHTML = estados[index];
+  estadosSelect.appendChild(estadoBr);
+}
