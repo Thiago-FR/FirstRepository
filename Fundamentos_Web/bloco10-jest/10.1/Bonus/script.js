c6f3aa526aa067c5d231102796c1729eb2581046
Thiago <thiago_ssfr@hotmail.com>
2021-10-01 17:11:45 -0300

    Requisito bonus exercicio 10.1

---
+++ Fundamentos_Web/bloco10-jest/10.1/Bonus/script.js
@@ -0,0 +1,51 @@
+// Dados
+const professionalBoard = [
+  {
+    id: '8579-6',
+    firstName: 'Ana',
+    lastName: 'Gates',
+    specialities: ['UX', 'Design'],
+  },
+  {
+    id: '5569-4',
+    firstName: 'George',
+    lastName: 'Jobs',
+    specialities: ['Frontend', 'Redux', 'React', 'CSS'],
+  },
+  {
+    id: '4456-4',
+    firstName: 'Leila',
+    lastName: 'Zuckerberg',
+    specialities: ['Context API', 'RTL', 'Bootstrap'],
+  },
+  {
+    id: '1256-4',
+    firstName: 'Linda',
+    lastName: 'Bezos',
+    specialities: ['Hooks', 'Context API', 'Tailwind CSS'],
+  },
+  {
+    id: '9852-2-2',
+    firstName: 'Jeff',
+    lastName: 'Cook',
+    specialities: ['Ruby', 'SQL'],
+  },
+  {
+    id: '4678-2',
+    firstName: 'Paul',
+    lastName: 'Dodds',
+    specialities: ['Backend'],
+  },
+];
+
+// Pesquisa
+const searchEmployee = (id, info) => {
+ const object = professionalBoard.find((obj) => id === obj.id);
+ if (!object) return "ID não identificada";
+ if (!object[info]) return "Informação indisponível";
+ return {id: object.id, [info]:object[info]}
+};
+
+console.log(searchEmployee('4678-2', 'done'));
+
+module.exports = searchEmployee;
