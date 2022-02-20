de9ef82ed2d281c11f37fdeed8835938528ec7a5
Thiago <thiago_ssfr@hotmail.com>
2022-02-20 19:35:07 -0300

    Exercicios Bonus

--- Back-End/block23-arquitetura-de-software-model/23.2/cep-lookup/models/Cep.js
+++ Back-End/block23-arquitetura-de-software-model/23.2/cep-lookup/models/Cep.js
@@ -1,26 +1,48 @@
 const connection = require('./connection');
+const fetch = require('node-fetch');
 
-const getCep = async (cep) => {
+const fetchApi = async (cep) => {
+  const result = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then(data => data.json());
+
+   return result;
+}
+
+const getCep = async (cepId) => {
   
   const [cepChecked] = await connection.execute(
     'SELECT * FROM cep_lookup.ceps ' +
-    'WHERE cep = ?', [cep]
+    'WHERE cep = ?', [cepId]
   );
+
+  if (!cepChecked.length) {
+    const apiExternal = await fetchApi(cepId);
+
+    if (!apiExternal.erro) {
+      const { logradouro, bairro, localidade, uf } = apiExternal;
+
+      await postCep({ cep: cepId, logradouro, bairro, localidade, uf });
+
+      return [{ cep: cepId, logradouro, bairro, localidade, uf }];
+    }
+  }
 
   return cepChecked;
 }
 
-const postCep = async (cepContent) => connection.execute(
-  'INSERT INTO cep_lookup.ceps (cep, logradouro, bairro, localidade, uf) ' +
-  'VALUES(?, ?, ?, ?, ?)',
-  [
-    cepContent.cep,
-    cepContent.logradouro,
-    cepContent.bairro,
-    cepContent.localidade,
-    cepContent.uf
-  ]
-);
+const postCep = async (cepContent) => {
+  connection.execute(
+    'INSERT INTO cep_lookup.ceps (cep, logradouro, bairro, localidade, uf) ' +
+    'VALUES(?, ?, ?, ?, ?)',
+    [
+      cepContent.cep,
+      cepContent.logradouro,
+      cepContent.bairro,
+      cepContent.localidade,
+      cepContent.uf
+    ]
+  )
+};
 
 module.exports = {
   getCep,
